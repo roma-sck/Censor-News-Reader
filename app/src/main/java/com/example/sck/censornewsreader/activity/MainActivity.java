cffa8f89da41651e66613fe8e58375e15e1c6681
roma.sck <roma.sck@gmail.com>
2015-06-19 16:25:01 +0300

    change icons, actionbar style

--- app/src/main/java/com/example/sck/censornewsreader/activity/MainActivity.java
+++ app/src/main/java/com/example/sck/censornewsreader/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.sck.censornewsreader.activity;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
@@ -10,7 +11,7 @@ import android.view.MenuItem;
 import com.example.sck.censornewsreader.R;
 import com.example.sck.censornewsreader.api.ApiRequest;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
