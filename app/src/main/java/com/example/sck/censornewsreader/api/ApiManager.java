07e75dd9e70c8d8fbf274fe89e602334c555d474
roma.sck <roma.sck@gmail.com>
2016-01-11 22:42:58 +0200

    temp java.security.cert.CertPathValidatorException solution by getting Unsafe OkHttpClient

--- app/src/main/java/com/example/sck/censornewsreader/api/ApiManager.java
+++ app/src/main/java/com/example/sck/censornewsreader/api/ApiManager.java
@@ -1,17 +1,72 @@
 package com.example.sck.censornewsreader.api;
 
+import com.squareup.okhttp.OkHttpClient;
+
+import java.security.cert.CertificateException;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
 import retrofit.RestAdapter;
+import retrofit.client.OkClient;
 
 public class ApiManager {
-
     /**
      * The RestAdapter class generates an implementation of the CensorAPI interface.
      */
-    private static final RestAdapter REST_ADAPTER = new RestAdapter.Builder().setEndpoint(Constants.API_URL).build();
+    private static final RestAdapter REST_ADAPTER = new RestAdapter.Builder()
+            .setEndpoint(Constants.API_URL)
+            .setClient(new OkClient(getUnsafeOkHttpClient()))
+            .build();
 
     private static final CensorApi CENSOR_NET_SERVICE = REST_ADAPTER.create(CensorApi.class);
 
     public static CensorApi getService() {
         return CENSOR_NET_SERVICE;
+    }
+
+    private static OkHttpClient getUnsafeOkHttpClient() {
+        try {
+            // Create a trust manager that does not validate certificate chains
+            final TrustManager[] trustAllCerts = new TrustManager[] {
+                    new X509TrustManager() {
+                        @Override
+                        public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+                        }
+
+                        @Override
+                        public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+                        }
+
+                        @Override
+                        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                            return null;
+                        }
+                    }
+            };
+
+            // Install the all-trusting trust manager
+            final SSLContext sslContext = SSLContext.getInstance("SSL");
+            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+            // Create an ssl socket factory with our all-trusting manager
+            final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+
+            OkHttpClient okHttpClient = new OkHttpClient();
+            okHttpClient.setSslSocketFactory(sslSocketFactory);
+            okHttpClient.setHostnameVerifier(new HostnameVerifier() {
+                @Override
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }
+            });
+
+            return okHttpClient;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
